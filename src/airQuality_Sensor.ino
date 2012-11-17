6a3a4647a51533debcc361094929968aba542e96
Jason <jason@jcode.me>
2012-11-17 17:25:44 +1030

    Bugfixes and alignment corrections

--- src/airQuality_Sensor.ino
+++ src/airQuality_Sensor.ino
@@ -11,7 +11,7 @@
 #include <Wire.h>
 #include <SPI.h>
 #include <stdlib.h>
-#include "<RTClib.h>"
+#include <RTClib.h>
 
 
 Adafruit_PCD8544 display = Adafruit_PCD8544(7, 6, 5, 4, 3);
@@ -20,9 +20,25 @@ RTC_DS1307 RTC;
 
 char buf[128];
 
+int dustPin       = 0;
+int ledPower      = 2;
+int delayTime     = 280;
+int delayTime2    = 40;
+int dustVal       = 0;
+
+float ppm         = 0;
+float offTime     = 9680;
+
+float voltage     = 0;
+float dustdensity = 0;
+float ppmpercf    = 0;
+
+
 void setup() {
   Wire.begin();
   RTC.begin();
+
+  pinMode(ledPower, OUTPUT);
 
   if (! RTC.isrunning()) {
     Serial.println("RTC is NOT running!");
@@ -45,6 +61,7 @@ void loop() {
   displayTemp();
   displayHumidity();
 
+  displayDustText();
   display.display();
   delay(2000);
 }
@@ -72,7 +89,10 @@ void displayTitle() {
 }
 
 void displayTemp() {
-  display.setCursor(0,11);
+  display.setCursor(0,19);
+
+  DHT22_ERROR_t errorCode;
+  errorCode = myDHT22.readData();
 
   sprintf(buf, "Temp:  %hi.%01hi$",
                 myDHT22.getTemperatureCInt()/10, abs(myDHT22.getTemperatureCInt()%10));
@@ -81,12 +101,60 @@ void displayTemp() {
 }
 
 void displayHumidity() {
-  display.setCursor(0,8);
+  display.setCursor(0,11);
+
+  DHT22_ERROR_t errorCode;
+  errorCode = myDHT22.readData();
 
   sprintf(buf, "Humid: %i.%01i%%",
                 myDHT22.getHumidityInt()/10, myDHT22.getHumidityInt()%10);
 
   display.println(buf);
+}
+
+void displayDustText() {
+  display.setCursor(0,27);
+
+  digitalWrite(ledPower,LOW);
+
+  delayMicroseconds(delayTime);
+
+  dustVal=analogRead(dustPin);
+  ppm = ppm+dustVal;
+
+  delayMicroseconds(delayTime2);
+
+  digitalWrite(ledPower,HIGH);
+
+  delayMicroseconds(offTime);
+
+  voltage       = ppm / 1 * 0.0049;
+  ppmpercf      = ( voltage - 0.0256 ) * 120000;
+  dustdensity   = 0.17 * voltage - 0.1;
+  ppm           = 0;
+
+  if ( ppmpercf < 0 ) {
+    ppmpercf = 0;
+  }
+
+  if ( ppmpercf < 75 ) {
+    sprintf(buf, "Dust:  None" );
+  } else if( ppmpercf > 75 && ppmpercf < 150 ) {
+    sprintf(buf, "Dust:  V Low" );
+  } else if( ppmpercf > 150 && ppmpercf < 300 ) {
+    sprintf(buf, "Dust:  Low" );
+  } else if( ppmpercf > 300 && ppmpercf < 1050 ) {
+    sprintf(buf, "Dust:  Med" );
+  } else if( ppmpercf > 1050 && ppmpercf < 3000   ) {
+    sprintf(buf, "Dust:  High" );
+  } else if ( ppmpercf < 3000 ) {
+    sprintf(buf, "Dust:  V High" );
+  } else {
+    sprintf(buf, "Unknown %i", ppmpercf);
+  }
+
+  display.println(buf);
+
 }
 
 void progressBar() {
